4251254ebad4272363a4cd1d0ac4974ce488d0a6
derinstaboy <90578108+derinstaboy@users.noreply.github.com>
2024-03-27 13:07:42 +0100

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -9,40 +9,38 @@ export default function Navbar(props) {
   const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
 
   return (
-    <>
-      <header className="primary-header">
-        <div className="container mobile-width">
-          <div className="nav-wrapper" id="nav-wrapper">
-            <div className="logo cursor-pointer">
-              <img src={Logo} alt="" />
-              <p>CroKing</p>
-            </div>
-
-            {/* Standard navigation buttons for desktop */}
-            <div className="nav-links">
-              <a href="https://app.croking.net/staking/boosted" className="nav-button">Staking</a>
-              <a href="https://app.croking.net/marketplace" className="nav-button">NFT Marketplace</a>
-            </div>
-
-            {/* Hamburger Icon for mobile */}
-            <div className="hamburger" onClick={toggleDropdown}>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-
-            {/* Dropdown Menu for mobile */}
-            {isDropdownOpen && (
-              <div className="dropdown-menu">
-                <a href="https://app.croking.net/staking/boosted">Staking</a>
-                <a href="https://app.croking.net/marketplace">NFT Marketplace</a>
-              </div>
-            )}
-
-            <ConnectButton sx={{ color: '#020202' }} />
+    <header className="primary-header">
+      <div className="container mobile-width">
+        <div className="nav-wrapper" id="nav-wrapper">
+          <div className="logo cursor-pointer">
+            <img src={Logo} alt="Logo" />
+            <p>CroKing</p>
           </div>
+
+          {/* Hamburger Icon for mobile */}
+          <div className="hamburger" onClick={toggleDropdown}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
+          {/* Desktop Navigation Links */}
+          <div className="nav-links">
+            <a href="https://app.croking.net/staking/boosted" className="nav-button">Staking</a>
+            <a href="https://app.croking.net/marketplace" className="nav-button">NFT Marketplace</a>
+          </div>
+
+          {/* Dropdown Menu for mobile */}
+          {isDropdownOpen && (
+            <div className="dropdown-menu">
+              <a href="https://app.croking.net/staking/boosted">Staking</a>
+              <a href="https://app.croking.net/marketplace">NFT Marketplace</a>
+            </div>
+          )}
+
+          <ConnectButton sx={{ color: '#020202' }} />
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 }
