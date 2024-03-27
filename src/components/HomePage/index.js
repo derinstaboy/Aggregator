1c765584added4a4234a017049a0ceda5449107f
derinstaboy <90578108+derinstaboy@users.noreply.github.com>
2024-03-27 13:41:45 +0100

    Update index.js

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -717,11 +717,6 @@ div className="slippage-display">
                       </p>
                     )}
                   </div>
-                    <div className="token-input-row">
-  <div className="amount-after-slippage">
-    Minimum received: {calculateAmountAfterSlippage()}
-  </div>
-</div>
                   <div
                     style={{
                       display: expanded ? "flex" : "none",
@@ -993,6 +988,11 @@ div className="slippage-display">
                         </div>
                     </div> */}
             </div>
+            <div className="token-input-row">
+  <div className="amount-after-slippage">
+    Minimum received: {calculateAmountAfterSlippage()}
+  </div>
+</div>
           )}
           <div className="img-wrapper">
             <img className="lionImage" src={lionImage} alt="lionImage" />
